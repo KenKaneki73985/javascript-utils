5b59d92586970fcb77de4682c08e5a43b654049f
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2025-08-23 21:22:44 +0800

    Update utils_ALL.js

--- utils_ALL.js
+++ utils_ALL.js
@@ -1,1 +1,19 @@
+// ==UserScript==
+// @name         ACTUAL utils ALL
+// @match        *://*/*
+// @require      https://raw.githubusercontent.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js
+// @require      https://raw.githubusercontent.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown_with_ms.js
+// @require      https://raw.githubusercontent.com/KenKaneki73985/javascript-utils/refs/heads/main/minified_javascript.js
+// ==/UserScript==
+// user_script = "" <--- This line is very important. Do not delete this at all cost.
 
+(function() {
+    'use strict'
+
+    // document.addEventListener('keydown', function(event){
+    //     if (event.altKey && event.key === 'k') {
+    //         SHOW_GUI("key has been pressed (include utils ALL)", "GUI_v1", "blue", 0, "y80", 17, 3000)
+    //     }
+    // })
+})()
+// August 23, 9:22 PM 2025
