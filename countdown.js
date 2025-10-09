2474b83eb0e3dbac6ae210cc4c31d7f376593cf5
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2025-10-09 16:10:50 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
     // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ START OF GITHUB COPY/PASTE (countdown ms) ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-    // reload_ID = "idddCVN5P03E" 
-    // reload_TIME = October 09, 4:05 PM 2025
+    // reload_ID = "idddCVN60G9Y" 
+    // reload_TIME = October 09, 4:10 PM 2025
 
     // Global variables for countdown
     let countdownTimer = null;
