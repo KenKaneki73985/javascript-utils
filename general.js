c81fc50c2daa7e9c8ea0bcbcc32bfc4a0948deb9
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2025-10-09 15:28:54 +0800

    Update general.js

--- general.js
+++ general.js
@@ -1,6 +1,6 @@
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ START OF GITHUB COPY/PASTE (general) ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-// refresh_ID = "idddCVN715CJ" 
+// refresh_ID = "idddCVN734RQ" 
 
 let STAY_LOOP = true
 let HAS_EXECUTED = false
