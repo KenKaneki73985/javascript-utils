3c6c4d726bc556fef9deac99797d5c63800b067b
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2025-10-10 19:44:02 +0800

    Update all_utils.js

--- all_utils.js
+++ all_utils.js
@@ -1,13 +1,39 @@
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ START OF GITHUB COPY/PASTE (all_utils) ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-// reload_ID = "idddCW3RABCV"
-// reload_TIME = October 10, 7:42 PM 2025
+// reload_ID = "idddCW3RDW9X"
+// reload_TIME = October 10, 7:44 PM 2025
 
 function all_utils_test(){
   alert("all utils October 10, 7:06 PM 2025")
 }
 
-// all_utils_test()
+function LoadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
+}
+
+// Load scripts sequentially
+Promise.all([
+    LoadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js'),
+    LoadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js'),
+    LoadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown.js')
+])
+.then(PromiseSuccess)
+.catch(PromiseError)
+
+function PromiseSuccess(){
+    show_GUI('☑️ success: all utils imported', "GUI_v1", "blue", 0, "y80", 17, 3000)
+    log('☑️ success: all utils imported')
+}
+function PromiseError(error){
+    alert("❌ error: script loading failed")
+    console.error('❌ error: script loading failed:' + error)
+}
 
 // const scripts = [
 //     'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js',
@@ -20,33 +46,5 @@ function all_utils_test(){
 //     script.src = src;
 //     document.head.appendChild(script);
 // });
-
-function loadScript(src) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.onload = resolve;
-        script.onerror = reject;
-        document.head.appendChild(script);
-    });
-}
-
-// Load scripts sequentially
-Promise.all([
-    loadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js'),
-    loadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js'),
-    loadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown.js')
-])
-.then(promise_success)
-.catch(promise_error)
-
-function promise_success(){
-    show_GUI('☑️ success: all utils imported', "GUI_v1", "blue", 0, "y80", 17, 3000)
-    log('☑️ success: all utils imported')
-}
-function promise_error(error){
-    alert("❌ error: script loading failed")
-    console.error('❌ error: script loading failed:' + error)
-}
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ END OF GITHUB COPY/PASTE ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
