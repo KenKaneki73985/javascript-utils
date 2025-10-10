983955bb56cf93eb74244e3c8477ffc73af63968
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2025-10-10 17:18:30 +0800

    Update all_utils.js

--- all_utils.js
+++ all_utils.js
@@ -1,1 +1,15 @@
+function all_utils_test(){
+  alert("im all utils")
+}
 
+const scripts = [
+    'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js',
+    'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js',
+    'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown.js'
+];
+
+scripts.forEach(src => {
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+});
