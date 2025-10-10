539f26c1122e711ab89a1e0d8fe17d27c39a22e3
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2025-10-10 21:28:35 +0800

    Update all_utils.js

--- all_utils.js
+++ all_utils.js
@@ -1,10 +1,30 @@
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ START OF GITHUB COPY/PASTE (all utils) ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-// reload_ID = "idddCW3ORRZN"
-// reload_TIME = October 10, 9:00 PM 2025
+// reload_ID = "idddCW3QG9W5"
+// reload_TIME = October 10, 9:28 PM 2025
 
 function all_utils_test(){
   alert("all utils October 10, 8:55 PM 2025")
+}
+
+document.readyState === 'complete' ? UTILS_PAGE_READY_ACTIONS() : addEventListener('load', UTILS_PAGE_READY_ACTIONS)
+
+function UTILS_PAGE_READY_ACTIONS(){
+    LoadUtils()
+}
+
+function LoadUtils(){
+    const scripts = [
+        'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js',
+        'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js',
+        'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown.js'
+    ]
+
+    scripts.forEach(src => {
+        const script = document.createElement('script');
+        script.src = src;
+        document.head.appendChild(script);
+    });
 }
 
 // function LoadScript(src) {
@@ -35,25 +55,52 @@ function all_utils_test(){
 //     console.error('❌ error: script loading failed:' + error)
 // }
 
-document.readyState === 'complete' ? UTILS_PAGE_READY_ACTIONS() : addEventListener('load', UTILS_PAGE_READY_ACTIONS)
 
-function UTILS_PAGE_READY_ACTIONS(){
-    LoadUtils()
-}
+// function LoadUtils(){
+//     // function LoadScript(src) {
+//     //     return new Promise((resolve, reject) => {
+//     //         const script = document.createElement('script');
+//     //         script.src = src;
+//     //         script.onload = resolve;
+//     //         script.onerror = reject;
+//     //         document.head.appendChild(script);
+//     //     });
+//     // }
 
-function LoadUtils(){
-    const scripts = [
-        'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js',
-        'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js',
-        'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown.js'
-    ]
+//     // // Load scripts sequentially
+//     // Promise.all([
+//     //     LoadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js'),
+//     //     LoadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js'),
+//     //     LoadScript('https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown.js')
+//     // ])
+//     // .then(PromiseSuccess)
+//     // .catch(PromiseError)
 
-    scripts.forEach(src => {
-        const script = document.createElement('script');
-        script.src = src;
-        document.head.appendChild(script);
-    });
-}
+//     // function PromiseSuccess(){
+//     //     // show_GUI('☑️ success: all utils imported', "GUI_v1", "blue", 0, "y80", 17, 3000)
+//     //     log('☑️ success: all utils imported')
+//     // }
+//     // function PromiseError(error){
+//     //     alert("❌ error: script loading failed")
+//     //     console.error('❌ error: script loading failed:' + error)
+//     // }
+
+//     const scripts = [
+//         'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/general.js',
+//         'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/show_GUI.js',
+//         'https://raw.githack.com/KenKaneki73985/javascript-utils/refs/heads/main/countdown.js'
+//     ]
+
+//     scripts.forEach(src => {
+//         const script = document.createElement('script');
+//         script.src = src;
+//         document.head.appendChild(script);
+//     });
+
+//     console.log('☑️ done import utils')
+//     // UtilsLoaded = true
+// }
+
 
 
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ END OF GITHUB COPY/PASTE ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
