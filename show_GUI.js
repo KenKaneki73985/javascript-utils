c83d1e4c5b7a0cf10c63d607678215585555e312
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2026-01-24 21:47:26 +0800

    Update show_GUI.js

--- show_GUI.js
+++ show_GUI.js
@@ -1,7 +1,7 @@
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
     // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ START OF GITHUB COPY/PASTE (show GUI) ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-    // reload_ID = "idddCW3QZ9UZ"
-    // reload_TIME = October 10, 10:37 PM 2025
+    // reload_ID = "iddd1L3409PA"
+    // reload_TIME = January 24, 9:47 PM 2026
     
     function show_GUI(text, GUI, color, extra_xpos, ypos, fontsize, time){
         // message.show_GUI("hello", "GUI_v1", "green", 0, "y80", 16, 3000)
