d172c5a19bdc54c3c202cacbbefb5473a8de175c
Ken Kaneki <123579565+KenKaneki73985@users.noreply.github.com>
2026-01-24 23:14:45 +0800

    Update general.js

--- general.js
+++ general.js
@@ -1,7 +1,7 @@
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ START OF GITHUB COPY/PASTE (general) ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-// reload_ID = "idddSE9N"
-// reload_TIME = January 24, 11:13 PM 2026
+// reload_ID = "idddUTHU"
+// reload_TIME = January 24, 11:14 PM 2026
 
 let STAY_LOOP = true
 let HAS_EXECUTED = false
